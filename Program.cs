c102fd9132c947e1cc98b802a1d3cf302797cc81
shefali <shefalis2708@gmail.com>
2022-05-06 18:39:37 +0530

    SinglePlayer - Position 0

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadderProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Snake and Ladder game played with single player at start position 0
+            Console.WriteLine("Hello to Snake And Ladder Game !");
+
+            UC1_SnakeAndLadder GameUC1 = new UC1_SnakeAndLadder();
+            GameUC1.Start_Game();
+        }
+    }
+}
---
+++ UC1_SnakeAndLadder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadderProgram
+{
+    //Snake and Ladder game played with single player at start position 0
+    class UC1_SnakeAndLadder
+    {
+        //Variable
+        public int Player_Position = 0;
+
+        public void Start_Game()
+        {
+            Console.WriteLine(" ----- Game Started ----- ");
+            Console.WriteLine("Number of Player is Single PLayer");
+            Console.WriteLine("Player Position : {0}", Player_Position);
+        }
+    }
+}
